a5070d07079991e3a00bac8571fa248df249434f
MD SAIFUL HOSSAIN <mdsaiful.rbs@gmail.com>
2024-09-14 20:54:02 +0600

    build sh added

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Clean the Flutter project
+flutter clean
+
+# Get Flutter dependencies
+flutter pub get
+echo "Building Android..."
+
+# Build the APK in release mode
+flutter build apk --release -t lib/main_dev.dart
+
+#get month
+month=$(date "+%m")
+
+#get day
+day=$(date "+%d")
+
+BUILD_NUMBER=1;
+
+# Copy the built APK to a new file with a timestamped filename
+ cp build/app/outputs/flutter-apk/app-release.apk "jttd_v${month}.${day}.${BUILD_NUMBER}.apk"
+
+# copy the built APK to the Desktop
+#cp build/app/outputs/flutter-apk/app-release.apk  ~/Desktop/sandra_v${month}.${day}.${BUILD_NUMBER}.apk
+
+# Output a message indicating that the build is complete
+echo "APK created successfully"
